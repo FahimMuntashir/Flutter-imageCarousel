9c93966e627d50d500a6f09f2226328afff5beb4
FahimMuntashir <fahim.tech.me@gmail.com>
2020-05-18 02:33:54 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,7 @@ class _HomePageState extends State<HomePage> {
                   top: 375.0,
                   left: 25.0,
                   right: 25.0,
-                  child: SelectedPhoto(),
+                  child: SelectedPhoto(numberOfDots: photos.length,photoIndex: photoIndex,),
                 ),
               ],
             ),
@@ -95,12 +95,70 @@ class _HomePageState extends State<HomePage> {
 
 class SelectedPhoto extends StatelessWidget {
 
-  final int numOfDots;
+  final int numberOfDots;
   final int photoIndex;
 
-  SelectedPhoto({this.numOfDots,this.photoIndex});
+  SelectedPhoto({this.numberOfDots, this.photoIndex});
+
+  Widget _inactivePhoto() {
+    return new Container(
+        child: new Padding(
+          padding: const EdgeInsets.only(left: 3.0, right: 3.0),
+          child: Container(
+            height: 8.0,
+            width: 8.0,
+            decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(4.0)
+            ),
+          ),
+        )
+    );
+  }
+
+  Widget _activePhoto() {
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.only(left: 3.0, right: 3.0),
+        child: Container(
+          height: 10.0,
+          width: 10.0,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5.0),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey,
+                    spreadRadius: 0.0,
+                    blurRadius: 2.0
+                )
+              ]
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _buildDots() {
+    List<Widget> dots = [];
+
+    for(int i = 0; i< numberOfDots; ++i) {
+      dots.add(
+          i == photoIndex ? _activePhoto(): _inactivePhoto()
+      );
+    }
+
+    return dots;
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return new Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: _buildDots(),
+      ),
+    );
   }
 }
